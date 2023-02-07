c18f2e24df0c4541a5b667477db00a4e7cf1929f
shahi1208 <61556068+shahi1208@users.noreply.github.com>
2023-02-07 17:06:15 +0530

    Add files via upload

--- etl-part/data_feed.py
+++ etl-part/data_feed.py
@@ -1,86 +1,101 @@
-import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask 
 import pandas as pd
-import datetime
-import json
-from sqlalchemy import create_engine
 import psycopg2
+from  sqlalchemy import create_engine
+import logging
 from data_validation import check_valid_data
 
-
-user_id = 'username'
-token = 'generated request token'
-
-
-if __name__ == "__main__":
-    headers = { 
-        "Accept" : "application/json" ,
-        "Content-Type" : "application/json" ,
-        "Authorization" : "Bearer {token}".format(token=token)
-    }
-
-    today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
-    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+# App config
+app = Flask(__name__)
 
 
-    r = requests.get('https://api.spotify.com/v1/me/player/recently-played?limit=20&after={time}'.format(time= yesterday_unix_timestamp),headers=headers)
-    data = r.json()
+app.secret_key = 'jvlxjfdhsfgdrht'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-cookie'
 
-    # print(data['items'][0]['track']['artists'][0]['name']) # artist name
-    # print(data['items'][0]['track']['name']) # song name
-    # print(data['items'][0]['track']['album']['name']) # album name
-    # print(data['items'][0]['played_at']) # played at
-    # print(data['items'][0]['played_at']) # played at date
+@app.route('/')
+def login():
+        return 'done'
 
-    artist_name = []
-    song_name = []
-    album_name = []
-    played_at = []
-    date = []
+logging.basicConfig(filename='data.log',filemode='w',level=logging.INFO)
 
-    for songs in data['items']:
-        artist_name.append(songs['track']['artists'][0]['name'])
-        song_name.append(songs['track']['name'])
-        album_name.append(songs['track']['album']['name'])
-        played_at.append(songs['played_at'])
-        date.append(songs['played_at'][0:10])
 
-    song_df = pd.DataFrame(
-        {'song_name':song_name,
-         'artist_name': artist_name,
-         'album_name':album_name,
-         'played_at' :played_at,
-         'date':date},
-         columns= ['song_name','artist_name','album_name','played_at','date'])
-
-    
-    if check_valid_data(song_df):
-        print("data is ready to upload")
-
-    database = 'postgresql+psycopg2://usrpw@host:port/dbname'
-
-    engine = create_engine(database)
-    conn = engine.raw_connection()
-    cursor = conn.cursor()
-
-    query = '''
-    CREATE TABLE songs (
-        song_name TEXT,
-        artist_name TEXT,
-        album_name TEXT,
-        played_at TEXT,
-        date DATE,
-        constraint played_at_pkey PRIMARY KEY (played_at)
-    )
-    '''
-
-    cursor.execute(query)
+if __name__ == '__main__':
 
     try:
-        song_df.to_sql('songs_table', engine, index=False, if_exists='append')
+
+        client_id='2e1afec1043741c3a5e81a77f52dc428'
+        client_secret='0e493fd1e669420a9963cc23b8368712'
+        redirect_uri=  'http://localhost:5000'
+
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                                    client_secret= client_secret,
+                                                    redirect_uri=redirect_uri,
+                                                    scope="user-read-recently-played"))
+
+        data = sp.current_user_recently_played()
+        logging.info('retrived data succesfully')
     except:
-        print('database already exists')
+        logging.error('trouble in pulling the data',exc_info=True)
 
-    conn.close()
 
-    
+    try:
+        artist_name = []
+        song_name = []
+        album_name = []
+        played_at = []
+        date = []
+
+        for songs in data['items']:
+            artist_name.append(songs['track']['artists'][0]['name'])
+            song_name.append(songs['track']['name'])
+            album_name.append(songs['track']['album']['name'])
+            played_at.append(songs['played_at'])
+            date.append(songs['played_at'][0:10])
+
+        song_df = pd.DataFrame(
+            {'song_name':song_name,
+            'artist_name': artist_name,
+            'album_name':album_name,
+            'played_at' :played_at,
+            'date':date},
+            columns= ['song_name','artist_name','album_name','played_at','date'])
+        logging.info('sucessfully transformed data')
+    except:
+        logging.error('error in formatting the data',exc_info=True)
+
+    try:
+        database = 'postgresql+psycopg2://shaistha:shaistha@localhost:5432/mydb'
+
+        engine = create_engine(database)
+        conn = engine.raw_connection()
+        cursor = conn.cursor()
+        logging.info('database setup done')
+    except:
+        logging.error('trouble in connecting the database',exc_info=True)
+
+
+    try:
+
+        if check_valid_data(song_df):
+
+            query = '''
+            CREATE TABLE songs (
+                song_name TEXT,
+                artist_name TEXT,
+                album_name TEXT,
+                played_at TEXT,
+                date DATE,
+                constraint played_at_pkey PRIMARY KEY (played_at)
+            )
+            '''
+
+            cursor.execute(query)
+
+            song_df.to_sql('songs_table', engine, index=False, if_exists='append')
+            conn.close()
+            logging.info('uploaded to database')
+    except:
+        logging.error('data already exists',exc_info=True)
+
